8eb62b756eff45a9179e4ea913969ac44ce244b9
sinaemre <snabkr7010@gmail.com>
2022-06-01 07:48:10 +0300

    Faktoriyel

---
+++ src/Faktoriyel.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Faktoriyel {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Faktöriyel Sayını Giriniz");
+        int sayi=sc.nextInt();
+        int toplam=1;
+        int sayac=1;
+
+        while (sayac <= sayi){
+            toplam*=sayac;
+            sayac++;
+        }
+        System.out.println( sayi+" sayısının faktöriyeli: "+toplam);
+    }
+}
